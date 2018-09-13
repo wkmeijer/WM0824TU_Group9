10ae9b9071c6c404f8e04285d9a3f4d3b7b9316f
Chris Berg <c.c.berg@student.tudelft.nl>
2018-09-14 00:49:56 +0200

    Matlab code to extract data and count frequencies

---
+++ metrics.m
@@ -0,0 +1,19 @@
+filename = 'data_miraibadpackets.csv';
+data = readtable(filename);
+asn = data( : , 4 );
+
+asn = categorical(table2array(asn));
+asn = table(categories(asn),countcats(asn));
+
+asn = sortrows(asn,2,'descend');
+asn = asn(1:10, :);
+
+asn_labels = table2array(asn(:, 1))';
+asn_values = table2array(asn(:, 2))';
+
+figure('rend','painters','pos',[0 0 1100 600]);
+bar(asn_values);
+set(gca,'xticklabel',asn_labels);
+xlabel(gca, 'Autonomous System Number, 10 most frequent.');
+ylabel(gca, 'Number of unique Mirai botnets targeting the ASN.');
+saveas(gcf,'frequently.png')
